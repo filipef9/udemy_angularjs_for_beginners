6671f03d506145d9222f3da4256b956cd8598489
Filipe dos Santos Nascimento <filipef9@hotmail.com>
2018-07-12 00:58:16 -0300

    Section 5 - Easy Navigation In Grocery List Using Routing

--- grocery_list/js/app.js
+++ grocery_list/js/app.js
@@ -1,10 +1,38 @@
-var app = angular.module("groceryListApp", []);
+var app = angular.module("groceryListApp", [
+    "ngRoute"
+]);
+
+app.config(function($routeProvider) {
+
+    $routeProvider
+
+        .when("/", {
+            templateUrl: "views/groceryList.html",
+            controller: "GroceryListItemsController"
+        })
+
+        .when("/addItem", {
+            templateUrl: "views/inputItem.html",
+            controller: "GroceryListItemsController"
+        })
+
+        .when("/addItem/:id", {
+            templateUrl: "views/inputItem.html",
+            controller: "GroceryListItemsController"
+        })
+
+        .otherwise({ redirectTo: "/" })
+});
 
 app.controller("HomeController", ["$scope", function($scope) {
     $scope.appTitle = 'Grocery List';
 }]);
 
-app.controller("GroceryListItemsController", ["$scope", function($scope) {
+app.controller("GroceryListItemsController", [
+    "$scope", 
+    "$routeParams",
+    function($scope, $routeParams) {
+
     $scope.groceryItems = [
         { completed: true, itemName: 'milk', date: '2018-07-01' },
         { completed: true, itemName: 'cookies', date: '2018-07-01' },
@@ -14,5 +42,7 @@ app.controller("GroceryListItemsController", ["$scope", function($scope) {
         { completed: true, itemName: 'bread', date: '2018-07-03' },
         { completed: true, itemName: 'eggs', date: '2018-07-04' },
         { completed: true, itemName: 'tortillas', date: '2018-07-04' },
-    ]
+    ];
+
+    $routeParams.id;
 }]);
